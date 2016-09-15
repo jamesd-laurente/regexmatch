ff05ccfe2e05731cc11152148a56733e155139a3
jdplaurente <jdplaurente@192.168.56.1>
2016-09-15 08:36:28 +0800

    regexmatch

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -2,6 +2,7 @@ package regexmatch;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import static java.time.Clock.system;
 import java.util.Scanner;
 
 public class Regexmatch 
@@ -16,8 +17,13 @@ public class Regexmatch
         System.out.println(isTrue("True"));
         System.out.println(containsFalse("jhflasehqsd"));
         Scanner sc = new Scanner(new FileReader("input.txt"));
+    
+        //reads the file "input.txt"
+        while(sc.hasNext())
+        {
+            System.out.println(sc.nextLine());
+        }
     }
-
     private static boolean containsFalse(String _) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
